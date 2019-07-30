ce87ccd2245ab52a7c96ec4af7d22b67f0d59a48
Solfrid Johansen <solfrid.johansen@hotmail.com>
2019-07-30 13:37:39 +0200

    Add php

--- index.php
+++ index.php
@@ -16,49 +16,8 @@
 </head>
 
 <body>
-<a class='head' href='index.html'>
-<div class="jumbotron text-center" style="margin-bottom:0">
-  <h1>NumProg</h1>
-  <p>Numerisk fysikk for fysikkemnene på NTNU</p>
-</div>
-</a>
 
-<nav class="navbar navbar-expand-sm navbar-dark bg-dark">
-  <!-- Brand -->
-
-  <a class="navbar-brand"></a>
-
-  <button class="navbar-toggler pull-right" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-
-  <div class="collapse navbar-collapse justify-content-center" id="collapsibleNavbar">
-
-  <!-- Links -->
-
-  <ul class="navbar-nav">
-    <li class="nav-item">
-      <a class="nav-link" href="index.html">Forside</a>
-    </li>
-    <li class="nav-item">
-      <a class="nav-link" href="laeringsressurser/laeringsressurser.html">Læringsressurser</a>
-    </li>
-
-    <!-- Dropdown -->
-    <li class="nav-item dropdown">
-      <a class="nav-link dropdown-toggle" href="#" id="navbardrop" data-toggle="dropdown">
-        Emner
-      </a>
-      <div class="dropdown-menu">
-        <a class="dropdown-item activate" href="emnesider/FY1001/FY1001.html">FY1001 Mekanisk fysikk</a>
-        <a class="dropdown-item activate" href="emnesider/FY1003/FY1003.html">FY1003 Elektrisitet og magnetisme</a>
-        <a class="dropdown-item activate" href="emnesider/TFY4163/TFY4163.html">TFY4163 Bølgefysikk og fluidmekanikk</a>
-      </div>
-    </li>
-  </ul>
-
-  </div>
-</nav>
+<?php include("navbar.php");?>
 
 <h1 class='headline'> Velkommen til NumProg <i class="fas fa-brain"></i></h1>
 
